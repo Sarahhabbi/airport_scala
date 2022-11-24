a65219a42a8b12d09aaf348c7e9d39d2f873fe5f
Sarahhabbi <habbisarah.1@gmail.com>
2022-11-24 08:24:51 +0100

    lecture fichier

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,22 @@
-class Main {
-  // Main Method
-  def main(args: Array[String]) {
-    // prints Hello World
-    println("Hello World!")
+import scala.io.Source
+import java.io.File
+
+
+class Main extends App {
+  val countries = "./src/main/data/countries.csv"
+  val airports = "./src/main/data/airports.csv"
+  val runways = "./src/main/data/runways.csv"
+
+  def readCSV(file_path: String): Unit = {
+    val file = new File(file_path)
+    val readBuffer = Source.fromFile(file)
+    readBuffer
+      .getLines()
+      .foreach(line => println(line))
+    readBuffer.close()
   }
+  readCSV(countries)
+  // readCSV(airports)
+  // readCSV(runways)
+
 }
