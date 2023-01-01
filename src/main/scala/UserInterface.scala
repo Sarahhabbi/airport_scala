7b9d4f8b06aac7241a710024f7b2328b79e3c70f
Sarahhabbi <habbisarah.1@gmail.com>
2023-01-01 17:41:15 +0100

    refactor code and add pseudo code for last functions

--- src/main/scala/UserInterface.scala
+++ src/main/scala/UserInterface.scala
@@ -1,40 +1,9 @@
-import Model.{airports, getCountryFromIso, getIsoFromCountry, iso_countries, loadAirports, loadCountries, loadRunways, runways}
+import Model.{loadAirports, loadCountries, loadRunways}
+import Service.{showCountry, showTop10Countries, showTop10RunwaysLatitude, showTypeRunways}
 
 import scala.annotation.tailrec
 
 object UserInterface {
-
-  def searchCountryAirportsRunways(country: String, byCode: Boolean): Unit = {
-    println(s"Results for $country")
-    var iso_country = country
-    if (!byCode) {
-       iso_country = getIsoFromCountry(country)
-    }
-
-    val selected_airports = Model.airports.getOrElse(iso_country, null)
-    selected_airports.foreach(airport => {
-      println(s"---> $airport \n")
-      val airportRef = airport.id.toString
-      val airportRunways = Model.runways.getOrElse(airportRef.slice(1, airportRef.length() - 1), null)
-      if(airportRunways!=null){
-        airportRunways.foreach(airportRunway => {
-          println(s"  >> $airportRunway \n")
-        })
-      }
-    })
-  }
-
-  /* QUERY OPTION */
-  def showCountry(byCode: Boolean = false): Unit = {
-    if(byCode){
-      println("Enter the country code")
-    }
-    else{
-      println("Enter the country name")
-    }
-    val country = scala.io.StdIn.readLine()
-    searchCountryAirportsRunways(country, byCode)
-  }
 
   def displayReportOptions(errorMessage: String = ""): Unit = {
     if (errorMessage != "") {
@@ -55,37 +24,6 @@ object UserInterface {
       case "_" => printMenu("ERROR: Your choice is not valid !")
     }
     printMenu()
-  }
-
-  /* REPORT OPTION */
-  def showTop10Countries() = {
-    val sortedAirports = Model.airports.toSeq.sortBy(_._2.length)
-    val lowest = sortedAirports.filter(_._2.length == 1)
-    println(s"Countries with lowest number of airports")
-    lowest.foreach(airport => {
-      val iso_code = airport._1
-      val country = getCountryFromIso(iso_code)
-      val count = airport._2.length
-      println(s"---> $country: $count")
-    })
-
-    val top10Highest = sortedAirports.takeRight(10)
-    println(s"TOP 10 countries with highest number of airports")
-    top10Highest.foreach(airport => {
-      val iso_code = airport._1
-      val country = getCountryFromIso(iso_code)
-      val count = airport._2.length
-      println(s"---> $country: $count")
-    })
-
-  }
-
-  def showTypeRunways() = {
-    /* TODO: showTypeRunways*/
-  }
-
-  def showTop10RunwaysLatitude() = {
-    /* TODO:showTop10RunwaysLatitude */
   }
 
   def displayQueryOptions(errorMessage: String = ""): Unit = {
